d71c8bd407f0569fd3e038c070b0507d2c9142c7
master <master@TeamCity>
2025-10-24 12:38:53 +0000

    general settings of 'Build' build configuration were updated (TeamCity change in 'netology-popov' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,7 +35,7 @@ project {
 object Build : BuildType({
     name = "Build"
 
-    artifactRules = "target/*.jar => ."
+    artifactRules = "target/*.jar => jars"
     publishArtifacts = PublishMode.SUCCESSFUL
 
     vcs {
